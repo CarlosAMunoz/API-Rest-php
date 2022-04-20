c65c1a2752bd1b1a81267f24fb8244156575d4da
munozcar20 <carlosmz243@gmail.com>
2022-04-19 22:56:43 -0500

    Update all documents

---
+++ api/usuarios.php
@@ -0,0 +1,23 @@
+<?php
+//localhost:3306/..../api/usuarios.php
+//Este archivo recibe todas las peticiones de nuestro cliente. 
+
+
+// Crear
+
+
+//Obtener un usuario 
+
+
+
+//Obtener todos los usuarios 
+
+
+//Actualizar un usuario 
+
+
+
+//Eliminar un usuario 
+
+
+?>
