01c08525d2f4f5e48948c369283faf6502567725
munozcar20 <carlosmz243@gmail.com>
2022-05-04 16:30:51 -0500

    Finaliza API Backend

--- registro-usuarios-backend/api/usuarios.php
+++ registro-usuarios-backend/api/usuarios.php
@@ -14,7 +14,7 @@
 
 //Le indicamos al cliente que lo que le envío es un json
 header("Content-Type: application/json");
-
+include_once("../clases/class-usuario.php");
 
 //Definimos variable para imprimir resultados
 $resultado["mensaje"] = "";
@@ -23,34 +23,43 @@ $resultado["mensaje"] = "";
 switch($_SERVER['REQUEST_METHOD']){
     case 'POST':
         $_POST = json_decode(file_get_contents('php://input'),true);
+        //Creamos una instancia del tipo usuario y le enviamos los datos que pide el constructor. 
+        $usuario = new Usuario($_POST["nombre"], $_POST["apellido"], $_POST["fechaNacimiento"], $_POST["pais"]);
+        $usuario->guardarUsuario();
 
         //Guardamos en un array asociativo la respuesta que queremos dar para posteriormente regresarla de manera esturcturada (Foramto JSON). 
         $resultado["mensaje"] = "Guardar usuario nuevo. Inforamtion: ".json_encode($_POST);
-        
+        echo json_encode($resultado);
     break;
     case 'GET':
         //So traemos un ID, es porque vamos a retornar un usuario, de lo contrario vamos a retornarlos todos.  
         if (isset($_GET['id'])) {
-            $resultado["mensaje"] = "Obtener usuario con el id ".$_GET['id'];
-
+            //Puedo llamar el método de esta manera porque la función de la clase es estática. 
+            Usuario::obtenerUsuario($_GET['id']);
         }else {
-            $resultado["mensaje"]= "Obtener todos los usuarios";
-
+            //Puedo llamar el método de esta manera porque la función de la clase es estática. 
+            Usuario::obtenerUsuarios();
         }
     break;
     case 'PUT':
         $_PUT = json_decode(file_get_contents('php://input'),true);
+        $usuario = new Usuario($_PUT['nombre'], $_PUT['apellido'], $_PUT['fechaNacimiento'], $_PUT['pais']);
+        $usuario->actualizarUsuario($_GET['id']);
+
         $resultado["mensaje"] =   "Actualizar el usuario con el id ".$_GET['id'].
                                     "   Information a actualizar ".json_encode($_PUT);
 
+                                    echo json_encode($resultado);                     
+
     break;
     case 'DELETE':
+        Usuario::eliminarUsuario($_GET['id']);
         $resultado["mensaje"] =  "Eliminar usuario con el id ".$_GET['id'];
-
+        echo json_encode($resultado);
     break;
 }
 
-echo json_encode($resultado);
+
 
 
 ?>
---
+++ registro-usuarios-backend/clases/class-usuario.php
@@ -0,0 +1,184 @@
+<?php
+//Clase Usuario
+
+class Usuario {
+    private $nombre;
+    private $apellido; 
+    private $fechaNacimiento;
+    private $pais; 
+
+
+    public function __construct($nombre, $apellido, $fechaNacimiento, $pais){
+        $this->nombre = $nombre;
+        $this->apellido = $apellido; 
+        $this->fechaNacimiento = $fechaNacimiento;
+        $this->pais = $pais; 
+    }
+
+
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of apellido
+     */ 
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * Set the value of apellido
+     *
+     * @return  self
+     */ 
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fechaNacimiento
+     */ 
+    public function getFechaNacimiento()
+    {
+        return $this->fechaNacimiento;
+    }
+
+    /**
+     * Set the value of fechaNacimiento
+     *
+     * @return  self
+     */ 
+    public function setFechaNacimiento($fechaNacimiento)
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of genero
+     */ 
+    public function getPais()
+    {
+        return $this->pais;
+    }
+
+    /**
+     * Set the value of genero
+     *
+     * @return  self
+     */ 
+    public function setPais($pais)
+    {
+        $this->pais = $pais;    
+
+        return $this;
+    }
+
+    
+    // Para guardar un nuevo elemento en nuestro array, necesitamos trar toda la información del JSON y sobreescribirla. De lo contrario el Json no quedará con la sitaxis correcta. 
+    public function guardarUsuario(){
+        //Treaemos la información del archivo json
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        //Guardamos la información en un arrray asociativo
+        $usuarios = json_decode($contenidoArchivo, true);
+        //Agregamos un elemento adicional a nuestro Array
+        $usuarios[] = array(
+
+                "usuario"=> $this->nombre,
+                "apellido"=> $this->apellido,
+                "fechaNacimiento"=> $this->fechaNacimiento,
+                "pais"=> $this->pais
+        );
+
+        //Abrimos el archivo haciendo referencia a su origen y el método w para sustituír. 
+        $archivo =fopen("../data/usuarios.json", "w");
+        //El método fwrite nos ayudará a escribir el documento con nuestro arreglo nuevo. 
+        fwrite($archivo, json_encode($usuarios));
+        //Finalmente cerramos el flujo del archivo. 
+        fclose($archivo);
+    }
+
+    //Cuando agregamos el modificador static, hace que la función pueda ser accedida sin crear una instancia. 
+    public static function obtenerUsuarios(){
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        echo $contenidoArchivo;
+    }
+
+    
+    public static function  obtenerUsuario($indice){
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        //true para que sea un arreglo asociativo.
+        $usuarios = json_decode($contenidoArchivo, true);
+        //Obtenemos e imprimimos un subarreglo del íncide indicado. 
+        echo json_encode($usuarios[$indice]);
+    }
+
+    public function actualizarUsuario($indice){
+        //Leemos el contenido del archivo. 
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        //Lo convertimos en un arreglo asociativo
+        $usuarios = json_decode($contenidoArchivo, true);
+  
+
+        $usuario = array(
+            'nombre'=>$this->nombre,
+            'apellido'=>$this->apellido,
+            'fechaNacimiento'=>$this->fechaNacimiento,
+            'pais'=>$this->pais
+        );
+
+        $usuarios[$indice] = $usuario;
+
+        $archivo = fopen("../data/usuarios.json", "w");
+        fwrite($archivo, json_encode($usuarios));
+        fclose($archivo);
+    }
+
+
+    public static function eliminarUsuario($indice){
+        //Leemos el contenido del archivo. 
+        $contenidoArchivo = file_get_contents("../data/usuarios.json");
+        //Lo convertimos en un arreglo asociativo
+        $usuarios = json_decode($contenidoArchivo, true);
+
+        array_splice($usuarios, $indice, 1);
+
+        $archivo = fopen("../data/usuarios.json", "w");
+        fwrite($archivo, json_encode($usuarios));
+        fclose($archivo);
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+?>
